230aa11329fe96518e293a84ba449623a864487c
zaire <409626581@qq.com>
2020-11-07 21:22:13 +0800

    fix

--- packages/zmi-test/src/test.ts
+++ packages/zmi-test/src/test.ts
@@ -1,1 +1,21 @@
-export default async function () {}
+import path from 'path'
+import fs from 'fs'
+import { assert } from '@zmi/utils'
+
+export default async function (args: any) {
+  process.env.NODE_ENV = 'test'
+
+  const cwd = args.cwd ?? process.cwd()
+
+  const userJestConfigFile = path.join(cwd, 'jest.config.js')
+  const userJestConfig =
+    fs.existsSync(userJestConfigFile) && require(userJestConfigFile)
+  assert(`config from jest.config.js: ${JSON.stringify(userJestConfig)}`)
+
+  const packageJSONPath = path.join(cwd, 'package.json')
+  const packageJestConfig =
+    fs.existsSync(packageJSONPath) && require(packageJSONPath).jest
+  assert(`jest config from package.json: ${JSON.stringify(packageJestConfig)}`)
+
+  
+}
