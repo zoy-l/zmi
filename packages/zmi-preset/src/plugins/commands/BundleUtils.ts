b6a590baa70537c8a599ddd81c4b01c13da4b983
zoy <409626581@qq.com>
2020-11-19 17:04:39 +0800

    fix

--- packages/zmi-preset/src/plugins/commands/BundleUtils.ts
+++ packages/zmi-preset/src/plugins/commands/BundleUtils.ts
@@ -1,3 +1,22 @@
-import {} from '@zmi/webpack'
+import { Bundler as DefaultBundler } from '@zmi/webpack'
 
-export function getBundleAndConfigs() {}
+export async function getBundleAndConfigs(options: {
+  api: any
+  port?: number
+}) {
+  const { api, number } = options
+
+  const Bundler = await api.applyPlugins({
+    type: api.ApplyPluginsType.modify,
+    key: 'modifyBundler',
+    initialValue: DefaultBundler
+  })
+
+  const bundleImplementor = await api.applyPlugins({
+    key: 'modifyBundleImplementor',
+    type: api.ApplyPluginsType.modify,
+    initialValue: undefined
+  })
+
+  
+}
