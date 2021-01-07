9baa3ac1eb084837d9125fda8cda46066dc27e1d
zoy <409626581@qq.com>
2021-01-07 16:11:41 +0800

    fix bugs

--- packages/zmi-build/src/getTsConifg.ts
+++ packages/zmi-build/src/getTsConifg.ts
@@ -1,3 +1,4 @@
+import { merge } from 'lodash'
 import ts from 'typescript'
 import path from 'path'
 import fs from 'fs'
@@ -11,6 +12,20 @@ export default function getTSConfig(cwd: string, pkgPath: string | undefined) {
 
   const readFile = (path: string) => fs.readFileSync(path, 'utf-8')
   const { error, config = {} } = ts.readConfigFile(tsconfig, readFile)
+
+  if (config.extends) {
+    const { error: exError, config: exConifg = {} } = ts.readConfigFile(
+      path.join(cwd, config.extends),
+      readFile
+    )
+
+    if (!exError) {
+      config.compilerOptions = merge(
+        exConifg.compilerOptions ?? {},
+        config.compilerOptions ?? {}
+      )
+    }
+  }
 
   return {
     tsConfig: config.compilerOptions ?? {
