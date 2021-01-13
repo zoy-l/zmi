122f00ec6b17d667ac0873f95810d2223fb700ef
zoy <409626581@qq.com>
2021-01-13 16:16:13 +0800

    Revert "fix bugs"
    This reverts commit 82d6c1866c97c9e9ab3d8dbf277acafa7e6ce752.

--- packages/zmi-babel-preset/node.js
+++ packages/zmi-babel-preset/node.js
@@ -1,4 +1,19 @@
 module.exports = function (opts) {
+  console.log([
+    require('./lib').default,
+    require('@zmi/utils').deepmerge(
+      {
+        typescript: true,
+        env: {
+          targets: {
+            node: 'current'
+          },
+          modules: 'commonjs'
+        }
+      },
+      opts
+    )
+  ])
   return {
     presets: [
       [
