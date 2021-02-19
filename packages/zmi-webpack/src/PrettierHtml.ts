2ab4d80565cb81de9c194b934d2ad4ba61a15f6f
zaire <409626581@qq.com>
2021-02-19 22:23:27 +0800

    fix:bugs

--- packages/zmi-webpack/src/PrettierHtml.ts
+++ packages/zmi-webpack/src/PrettierHtml.ts
@@ -3,6 +3,8 @@ import { Compiler } from 'webpack'
 import prettier from 'prettier'
 
 export default class PrettierHtml {
+  isInit = false
+
   apply(compiler: Compiler) {
     compiler.hooks.compilation.tap('PrettierHtml', (compilation) => {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
@@ -11,12 +13,17 @@ export default class PrettierHtml {
           // prettier the content
           // Node 8 not support prettier v2
           // https://github.com/prettier/eslint-plugin-prettier/issues/278
-          try {
-            data.html = prettier.format(data.html, {
-              parser: 'html'
-            })
-          } catch {
-            // ig-ignore
+          // It only needs to be done once
+          if (!this.isInit) {
+            try {
+              data.html = prettier.format(data.html, {
+                parser: 'html'
+              })
+            } catch {
+              // ig-ignore
+            }
+
+            this.isInit = true
           }
 
           cb(null, data)
