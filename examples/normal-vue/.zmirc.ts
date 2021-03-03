9ed6c006d309d752bb622f0daf01c1fa3cda7a72
zaire <409626581@qq.com>
2021-03-04 03:47:05 +0800

    fix: examples

--- examples/normal-vue/.zmirc.ts
+++ examples/normal-vue/.zmirc.ts
@@ -6,6 +6,6 @@ export default {
       content: 'zmi, zmi-cli'
     }
   ],
-  scripts: [`console.log(111);`, { src: ``, defer: true }],
+  // scripts: [`console.log(111);`, { src: ``, defer: true }],
   styles: [`body { color: red; }`]
 }
--- packages/zmi-utils/src/clearConsole.ts
+++ packages/zmi-utils/src/clearConsole.ts
@@ -1,7 +1,5 @@
-function clearConsole() {
+export default function clearConsole() {
   if (process.env.ZMI_TEST !== 'true') {
     process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
   }
 }
-
-export default clearConsole
