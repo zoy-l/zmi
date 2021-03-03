8781a65601ba589b5015b809dcf883c32a69a983
l-zoy <409626581@qq.com>
2021-03-03 23:55:41 +0800

    other:fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,1 +1,7 @@
-export default {}
+import { jestConfig } from 'zmi-nerd'
+
+export default {
+  collectCoverageFrom(memo) {
+    return memo.concat(['!packages/zmi-preset/src/plugins/features/*'])
+  }
+} as jestConfig
