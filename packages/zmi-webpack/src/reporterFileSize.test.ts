dbff4206a04fabde476abda628160d022d222db0
DESKTOP-7L1N1UL\Administrator <409626581@qq.com>
2021-03-07 07:15:27 +0800

    fix:test win path

--- packages/zmi-webpack/src/reporterFileSize.test.ts
+++ packages/zmi-webpack/src/reporterFileSize.test.ts
@@ -1,11 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+import { lodash } from '@zmi-cli/utils'
+import { rmdirSync } from 'fs-extra'
+import stripAnsi from 'strip-ansi'
+import gzipSize from 'gzip-size'
+import filesize from 'filesize'
+import path from 'path'
+import fs from 'fs'
 
 import { printFileSizesAfterBuild, measureFileSizesBeforeBuild } from './reporterFileSize'
-import { rmdirSync } from 'fs-extra'
-import { lodash, slash } from '@zmi-cli/utils'
-import stripAnsi from 'strip-ansi'
-import path from 'path'
 
 const state = {
   assetsByChunkName: { main: ['main.86a8dd21.css', 'main.abc02671.js'] },
@@ -27,51 +28,6 @@ const state = {
       chunkNames: ['main'],
       chunkIdHints: [],
       auxiliaryChunkNames: [],
-      auxiliaryChunkIdHints: []
-    },
-    {
-      type: 'asset',
-      name: 'chunk.abc02671.js',
-      size: 132365,
-      emitted: true,
-      comparedForEmit: false,
-      cached: false,
-      info: {
-        immutable: true,
-        contenthash: 'abc02671',
-        javascriptModule: false,
-        minimized: true,
-        size: 132365
-      },
-      chunkNames: ['chunk'],
-      chunkIdHints: [],
-      auxiliaryChunkNames: [],
-      auxiliaryChunkIdHints: []
-    },
-    {
-      type: 'asset',
-      name: 'index.html',
-      size: 3219,
-      emitted: true,
-      comparedForEmit: false,
-      cached: false,
-      info: { size: 3219 },
-      chunkNames: [],
-      chunkIdHints: [],
-      auxiliaryChunkNames: [],
-      auxiliaryChunkIdHints: []
-    },
-    {
-      type: 'asset',
-      name: 'img/logo.103b5fa1.svg',
-      size: 2671,
-      emitted: true,
-      comparedForEmit: false,
-      cached: false,
-      info: { immutable: true, sourceFilename: 'src/logo.svg', size: 2671 },
-      chunkNames: [],
-      chunkIdHints: [],
-      auxiliaryChunkNames: ['main'],
       auxiliaryChunkIdHints: []
     },
     {
@@ -102,51 +58,116 @@ afterEach(() => {
   window.console.log = cacheLog
 })
 
+const FIFTY_KILOBYTES = 1024 * 50
+
+const formatTest = (log: string[]) => {
+  const curVariety: number[] = []
+  const units: string[] = []
+  const logs = log.map((str) => {
+    const test = stripAnsi(str[0])
+      .replace(/reporter-file-size(\\|\/)/, '')
+      .split(' ')
+      .filter((t) => {
+        if (!t) {
+          return false
+        }
+        if (path.extname(t) === '.js' || path.extname(t) === '.css') {
+          return true
+        }
+
+        if (!isNaN(Number(t))) {
+          return true
+        }
+
+        if (t.split('')[0] === '(') {
+          curVariety.push(Number(t.split('(')[1]))
+        }
+
+        if (t === 'KB' || t === 'B') {
+          units.push(t)
+        }
+
+        return false
+      })
+
+    return test
+  })
+
+  units.forEach((unit, i) => {
+    if (unit === 'KB') {
+      curVariety[i] *= 1024
+    }
+  })
+
+  return {
+    logs,
+    curVariety
+  }
+}
+
 test('reporter file size null', () => {
   const sizes = { 'main.css': 304, 'main.js': 123007 }
   const _state = lodash.cloneDeep(state)
 
+  // @ts-expect-error test
   _state.assets = undefined
   printFileSizesAfterBuild(_state, sizes, fixtures)
 })
 
 test('reporter file size green', () => {
-  const sizes = { 'main.css': 304, 'main.js': 523007 }
+  const sizes = { 'main.css': 306, 'main.js': 523007 }
   printFileSizesAfterBuild(state, sizes, fixtures)
 
-  expect(
-    console.log.mock.calls.map((str) => stripAnsi(str[0]).replace(/reporter-file-size(\\|\/)/, ''))
-  ).toEqual([
-    '➜  main.abc02671.js  492.92 KB (-17.83 KB) ',
-    '➜  chunk.abc02671.js  82.99 KB              ',
-    '➜  main.86a8dd21.css  304 B                 '
-  ])
+  const received: string[][] = []
+  state.assets.forEach((asset) => {
+    const fileContents = fs.readFileSync(path.join(fixtures, asset.name))
+    const size = gzipSize.sync(fileContents)
+
+    received.push([asset.name, filesize(size).split(' ')[0]])
+  })
+  // @ts-expect-error test
+  const { logs, curVariety } = formatTest(console.log.mock.calls)
+
+  expect(JSON.stringify(received)).toEqual(JSON.stringify(logs))
+  expect(curVariety[0] < 0).toEqual(true)
+  expect(curVariety[1] < 0).toEqual(true)
 })
 
 test('reporter file size yellow', () => {
-  const sizes = { 'main.css': 50, 'main.js': 443007 }
+  const sizes = { 'main.css': 50, 'main.js': 464300 }
   printFileSizesAfterBuild(state, sizes, fixtures)
 
-  expect(
-    console.log.mock.calls.map((str) => stripAnsi(str[0]).replace(/reporter-file-size(\\|\/)/, ''))
-  ).toEqual([
-    '➜  main.abc02671.js  492.92 KB (+60.3 KB) ',
-    '➜  chunk.abc02671.js  82.99 KB             ',
-    '➜  main.86a8dd21.css  304 B (+254 B)       '
-  ])
+  const received: string[][] = []
+  state.assets.forEach((asset) => {
+    const fileContents = fs.readFileSync(path.join(fixtures, asset.name))
+    const size = gzipSize.sync(fileContents)
+
+    received.push([asset.name, filesize(size).split(' ')[0]])
+  })
+  // @ts-expect-error test
+  const { logs, curVariety } = formatTest(console.log.mock.calls)
+
+  expect(JSON.stringify(received)).toEqual(JSON.stringify(logs))
+  expect(curVariety[0] < FIFTY_KILOBYTES && curVariety[0] > 0).toEqual(true)
+  expect(curVariety[1] < FIFTY_KILOBYTES && curVariety[1] > 0).toEqual(true)
 })
 
 test('reporter file size red', () => {
-  const sizes = { 'main.css': 1024, 'main.js': 100 }
+  const sizes = { 'main.css': 1024, 'main.js': 243007 }
   printFileSizesAfterBuild(state, sizes, fixtures)
 
-  expect(
-    console.log.mock.calls.map((str) => stripAnsi(str[0]).replace(/reporter-file-size(\\|\/)/, ''))
-  ).toEqual([
-    '➜  main.abc02671.js  492.92 KB (+492.83 KB) ',
-    '➜  chunk.abc02671.js  82.99 KB               ',
-    '➜  main.86a8dd21.css  304 B (-720 B)         '
-  ])
+  const received: string[][] = []
+  state.assets.forEach((asset) => {
+    const fileContents = fs.readFileSync(path.join(fixtures, asset.name))
+    const size = gzipSize.sync(fileContents)
+
+    received.push([asset.name, filesize(size).split(' ')[0]])
+  })
+  // @ts-expect-error test
+  const { logs, curVariety } = formatTest(console.log.mock.calls)
+
+  expect(JSON.stringify(received)).toEqual(JSON.stringify(logs))
+  expect(curVariety[0] > FIFTY_KILOBYTES).toEqual(true)
 })
 
 const state2 = {
@@ -197,27 +218,8 @@ test('main suggest bundle splitting', () => {
   const sizes = { 'main.css': 1024, 'main.js': 1000 }
   printFileSizesAfterBuild(state2, sizes, fixtures)
 
-  expect(
-    console.log.mock.calls.map((str) => stripAnsi(str[0])?.replace(/reporter-file-size(\\|\/)/, ''))
-  ).toEqual([
-    '➜  main.abc02672.js  599.39 KB (+598.41 KB) ',
-    '➜  chunk.abc02672.js  599.39 KB              ',
-    undefined,
-    'The bundle size is significantly larger than recommended.',
-    'You can also analyze the project dependencies: https://webpack.js.org/guides/code-splitting/'
-  ])
-})
-
-test('chunk suggest bundle splitting', () => {
-  const sizes = { 'main.css': 1024, 'main.js': 600000 }
-  printFileSizesAfterBuild(state2, sizes, fixtures)
-
-  expect(
-    console.log.mock.calls.map((str) => stripAnsi(str[0])?.replace(/reporter-file-size(\\|\/)/, ''))
-  ).toEqual([
-    '➜  main.abc02672.js  599.39 KB (+13.45 KB) ',
-    '➜  chunk.abc02672.js  599.39 KB             ',
-    undefined,
+  // @ts-expect-error test
+  expect(console.log.mock.calls.map((str) => stripAnsi(str[0])).slice(-2)).toEqual([
     'The bundle size is significantly larger than recommended.',
     'You can also analyze the project dependencies: https://webpack.js.org/guides/code-splitting/'
   ])
